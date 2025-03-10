751577632f73455b17fd8f951867c866f8bb3ac4
bodarey <69037258+bodarey@users.noreply.github.com>
2025-03-10 13:28:11 +0000

    transform to json

---
+++ proj/homework 1/ab_json.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'pg'
+require 'json'
+require_relative 'library'
+hh = []
+# puts str
+
+# Connection parameters
+conn_params = {
+  host: 'localhost',  # Replace with your database host
+  dbname: 'postgres', # Replace with your database name
+  user: 'postgres',   # Replace with your database username
+  password: 'fraer'   # Replace with your database password
+}
+
+begin
+  # Establish a connection to the database
+  conn = PG.connect(conn_params)
+
+  # Execute a SELECT query
+  # result = conn.exec('select * from categories')
+  1.upto(8) do |i|
+    result = conn.exec(Library.query(i))
+   
+
+    # Iterate over the result set and print each row
+    result.each do |row|
+      hh << row.to_json
+      
+    end
+  end
+rescue PG::Error => e
+  puts "An error occurred: #{e.message}"
+ensure
+  # Close the connection
+  conn&.close
+end
+
+puts hh.to_json
